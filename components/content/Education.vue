704b3d2fb341ba4f680ab998d77af948ae6bb8e0
Vaelot <minusgodkeanu@gmail.com>
2019-09-11 15:25:41 +0900

    remove placeholder data and add fail-safe content

--- components/content/Education.vue
+++ components/content/Education.vue
@@ -7,14 +7,11 @@
         th.blue-grey.lighten-4.text-center File
         th.blue-grey.lighten-4.text-center Speaker
     tbody
-      tr(
-        v-for="item in items"
-        :key="item.t"
-      )
+      tr(v-for="(item, i) in items" :key="i")
         td.text-center {{ item.d }}
         td.text-center {{ item.t }}
         td.text-center
-          v-icon(:href="item.f") mdi-file-download-outline
+          v-icon(v-if="item.f" :href="item.f") mdi-file-download-outline
         td.text-center {{ item.s }}
 </template>
 
@@ -29,16 +26,9 @@ export default {
   data () {
     return {
       items: [
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' },
-        { d: new Date(), t: 'hello??', f: 'https://localhost:8000/api/help', s: '1' }
+        { d: '', t: '교육자료 목록을 가져오는데 실패하였습니다.', f: '', s: '' },
+        { d: '', t: '새로고침을 하여 다시 가져오시기 바랍니다.', f: '', s: '' },
+        { d: '', t: '새로고침을 하여도 실패할 경우, 관리자에게 연락 바랍니다.', f: '', s: '' }
       ]
     }
   }/*,
